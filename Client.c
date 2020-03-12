a97c943a303c21ab72e653167aeca07670e86bd4
kartikSakhuja <kartik.sakhuja@gmail.com>
2020-03-12 13:57:05 -0400

    Add files via upload

---
+++ Client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 4444
+
+void main(){
+	
+	int clientSocket;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
+
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if(clientSocket)
+	{
+	printf("[Servercode:0] Client Socket Created Sucessfully.\n");
+	}
+	else
+	{
+	printf("[Servercode:1] Client Socket not Created Sucessfully.\n")
+	}
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	printf("[+]Connected to Server.\n");
+
+	recv(clientSocket, buffer, 1024, 0);
+	printf("[+]Data Recv: %s\n", buffer);
+	printf("[+]Closing the connection.\n");
+
+}
